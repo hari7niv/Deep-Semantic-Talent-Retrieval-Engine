96e5f9fc92e073502f8e813b2ca33a798db882e0
hari7niv <hari7niv@gmail.com>
2026-06-28 12:00:11 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import json
+with open('C:\\Users\\Hari\\Desktop\\Projects\\AI\\India runs 2026\\[PUB] India_runs_data_and_ai_challenge\\[PUB] India_runs_data_and_ai_challenge\\India_runs_data_and_ai_challenge\\candidates.jsonl', 'r') as file:
+    data = json.load(file)
+
